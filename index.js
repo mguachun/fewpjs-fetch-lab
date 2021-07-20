6d1fac9c7b820181bcf18cfbb19d48b9ef291df0
Melissa Guachun <1212malka@gmail.com>
2021-07-20 12:29:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(json)); 
 }
 
 function renderBooks(books) {
@@ -9,9 +11,9 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
